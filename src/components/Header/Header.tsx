842a77756d29bb0a03cb9f05337608585bd1312b
Ysabella-dev <ysabellacristina.almeida@gmail.com>
2023-05-11 20:34:52 -0300

    deploy

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,26 @@
 import styles from "./header.module.scss";
-import logoImg from "../../assets/logo.svg";
+import logoImg from "../../assets/Logo.svg";
 import { useState } from "react";
 import Modal from "react-modal";
 
-interface IHeaderProps{
+interface IHeaderProps {
     onOpenNewTransationModal: () => void;
     // componente que receb a função: ON
 }
 
-export default function Header({onOpenNewTransationModal}:IHeaderProps){
+export default function Header({ onOpenNewTransationModal }: IHeaderProps) {
 
-    return(
+    return (
         <header className={styles.header_blue}>
             <div className={styles.div}>
-                <img src={logoImg} alt="logo dt money" />
+                <div className={styles.teste}>
+                    <img src={logoImg} alt="logo TransactionLogger" />
+                    <h1>Transaction Logger</h1>
+                </div>
                 <button type="button" onClick={onOpenNewTransationModal}>
                     Nova Transação
                 </button>
-                
+
             </div>
         </header>
     )
