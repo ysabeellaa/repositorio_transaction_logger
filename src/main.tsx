260ff773a9ab5a285bb92a7ac9b1c486d1ee6b1c
Ysabella-dev <ysabellacristina.almeida@gmail.com>
2023-05-13 17:41:34 -0300

    deploy test

--- src/main.tsx
+++ src/main.tsx
@@ -17,18 +17,22 @@ createServer({
   },
 
   routes() {
-    this.namespace = 'api' /* na chamada tem o api na url */
-    this.get('/transactions', () => {
-      return this.schema.all('transaction')
-      //retornar todos os dados do model transaction, todas as transações do banco de dados
-    })
-    // criar rota de listagem;request: dadosque envia para transactionsb. scheame: banco de dados (qual o model, dados que passa para o model)
+    this.namespace = 'api'; /* na chamada tem o api na url */
+    console.log(this.namespace);
+    this.get('/transactions', (schema) => {
+      return schema.all('transaction');
+      // Retorna todos os dados do model 'transaction', todas as transações do banco de dados
+    });
+  
     this.post('/transactions', (schema, request) => {
-      const data = JSON.parse(request.requestBody)
-      return schema.create('transaction', data)
-    })
+      const data = JSON.parse(request.requestBody);
+      return schema.create('transaction', data);
+    });
   }
+  
 })
+
+
 
 
 
