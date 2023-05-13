dba0d82f9181ac3e8c2e5f1c324493ce1d0b8ed1
Ysabella-dev <ysabellacristina.almeida@gmail.com>
2023-05-13 17:11:42 -0300

    deploy test

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 // criar uma isntancia para setar informações padrão para todas as requisições
 export const api = axios.create({
-    baseURL: 'http://localhost:5173/api',
+    baseURL: (import.meta.env.VITE_API_URL),
 })
